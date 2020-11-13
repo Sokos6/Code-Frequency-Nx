449ba35e7ac3a51d3b082c819525a6afd8f6b59d
Sokos6 <will.sokolowski@gmail.com>
2020-11-13 16:57:42 -0600

    global styles

--- libs/ui/src/lib/global-styles/global-styles.tsx
+++ libs/ui/src/lib/global-styles/global-styles.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { createGlobalStyle } from 'styled-components';
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
@@ -9,12 +9,23 @@ const StyledGlobalStyles = styled.div`
   color: pink;
 `;
 
-export const GlobalStyles = (props: GlobalStylesProps) => {
-  return (
-    <StyledGlobalStyles>
-      <h1>Welcome to GlobalStyles!</h1>
-    </StyledGlobalStyles>
-  );
-};
+// export const GlobalStyles = (props: GlobalStylesProps) => {
+//   return (
+//     <StyledGlobalStyles>
+//       <h1>Welcome to GlobalStyles!</h1>
+//     </StyledGlobalStyles>
+//   );
+// };
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-size: 16px;
+    font-family: sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 export default GlobalStyles;
