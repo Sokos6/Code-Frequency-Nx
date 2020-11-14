0cbc1c5703a88783bf554984ae377f7d779f5fe1
Sokos6 <will.sokolowski@gmail.com>
2020-11-13 20:52:49 -0600

    header

--- libs/ui/src/lib/header/header.tsx
+++ libs/ui/src/lib/header/header.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-
+import React, { HTMLAttributes } from 'react';
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
 export interface HeaderProps {}
 
 const StyledHeader = styled.div`
-  color: pink;
+  padding: 1rem;
+  background-color: #2657ba;
+  color: white;
+  display: flex;
+  align-items: center;
+
+  a {
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  > h1 {
+    margin: 0 1rem 0 0;
+    padding-right: 1rem;
+    border-right: 1px solid white;
+  }
 `;
 
-export const Header = (props: HeaderProps) => {
-  return (
-    <StyledHeader>
-      <h1>Welcome to Header!</h1>
-    </StyledHeader>
-  );
-};
+export const Header = (props: HTMLAttributes<HTMLElement>) => (
+  <StyledHeader>{props.children}</StyledHeader>
+);
 
 export default Header;
