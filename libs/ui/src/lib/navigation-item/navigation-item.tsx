d625492aa451653f0a14beb2cf5a4a745fca3f03
Sokos6 <will.sokolowski@gmail.com>
2020-11-13 21:19:18 -0600

    actual nav item

--- libs/ui/src/lib/navigation-item/navigation-item.tsx
+++ libs/ui/src/lib/navigation-item/navigation-item.tsx
@@ -1,20 +1,15 @@
-import React from 'react';
-
+import React, { LiHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
 export interface NavigationItemProps {}
 
-const StyledNavigationItem = styled.div`
-  color: pink;
+const StyledNavigationItem = styled.li`
+  margin-right: 1rem;
 `;
 
-export const NavigationItem = (props: NavigationItemProps) => {
-  return (
-    <StyledNavigationItem>
-      <h1>Welcome to NavigationItem!</h1>
-    </StyledNavigationItem>
-  );
+export const NavigationItem = (props: LiHTMLAttributes<HTMLLIElement>) => {
+  return <StyledNavigationItem>{props.children}</StyledNavigationItem>;
 };
 
 export default NavigationItem;
